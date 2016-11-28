d424a843d2b67459dbde998e6571b8b05b556932
Halfwit <michaelmisch1985@gmail.com>
2016-11-28 09:24:43 -0700

    Adding 2.5 second default timeout for whole operation

--- content-type.c
+++ content-type.c
@@ -6,104 +6,124 @@
 #include <ctype.h>
 #include <curl/curl.h>
 
-struct memory_struct {
-  char    *memory;
-  size_t  size;
+enum {
+	TIMEOUT_MS  = 2500,
+	MAX_REDIRS  = 5L
 };
 
-static size_t ct_strstr(char *in) {
+struct memory_struct {
+	char	*memory;
+	size_t	size;
+};
 
-  int i;
-
-  for (i = 0; in[i]; i++) {
-    in[i] = tolower(in[i]);
-  }
-
-  char *r = strstr(in, "content-type");
-  return r - in;
-
-}
-
-static size_t wm_callback(void *contents, size_t size, size_t nmemb, void *userp) {
-
-  size_t realsize = size * nmemb;
-  struct memory_struct *mem = (struct memory_struct *)userp;
-  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
-  if (mem->memory == NULL) {
-    fprintf(stderr, "Memory allocation error\n");
-    return 0;
-  }
-
-  memcpy(&(mem->memory[mem->size]), contents, realsize);
-  mem->size += realsize;
-  mem->memory[mem->size] = 0;
-
-  return realsize;
-}
-
-int main(int argc, char *argv[])
+static size_t
+ct_strstr(char *in)
 {
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <url>\n", argv[0]);
-  }
 
-  CURLcode ret;
-  CURL *hnd;
+	int i;
 
-  struct memory_struct chunk;
+	for (i = 0; in[i]; i++) {
+		in[i] = tolower(in[i]);
+	}
 
-  chunk.memory = malloc(1);
-  chunk.size = 0;
+	char *r = strstr(in, "content-type");
 
-  hnd = curl_easy_init();
+	return r - in;
 
-  curl_easy_setopt(hnd, CURLOPT_URL, argv[1]);
-  curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, wm_callback);
-  curl_easy_setopt(hnd, CURLOPT_WRITEDATA, (void *)&chunk);
-  curl_easy_setopt(hnd, CURLOPT_FAILONERROR, 1L);
-  curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
-  curl_easy_setopt(hnd, CURLOPT_NOBODY, 1L);
-  curl_easy_setopt(hnd, CURLOPT_HEADER, 1L);
-  curl_easy_setopt(hnd, CURLOPT_USERAGENT, "curl/7.50.1");
-  curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 5L);
-  curl_easy_setopt(hnd, CURLOPT_FOLLOWLOCATION, 1L);
-  curl_easy_setopt(hnd, CURLOPT_FILETIME, 1L);
-  curl_easy_setopt(hnd, CURLOPT_TCP_KEEPALIVE, 1L);
+}
 
-  ret = curl_easy_perform(hnd);
+static size_t
+wm_callback(void *contents, size_t size, size_t nmemb, void *userp)
+{
 
-  if(ret != CURLE_OK) {
-    fprintf(stderr, "Failed to fetch content-type, %s\n", curl_easy_strerror(ret));
-  } else {
+	size_t realsize = size * nmemb;
+	struct memory_struct *mem = (struct memory_struct *)userp;
+	mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+	
+	if (mem->memory == NULL) {
+		fprintf(stderr, "Memory allocation error\n");
+		return 0;
+	}
+
+	memcpy(&(mem->memory[mem->size]), contents, realsize);
+	mem->size += realsize;
+	mem->memory[mem->size] = 0;
+
+	return realsize;
+
+}
+
+int
+main(int argc, char *argv[])
+{
+	if (argc != 2)
+		fprintf(stderr, "Usage: %s <url>\n", argv[0]);
+
+	CURLcode ret;
+	CURL *hnd;
+
+	struct memory_struct chunk;
+
+	chunk.memory = malloc(1);
+	chunk.size = 0;
+
+	hnd = curl_easy_init();
+
+	curl_easy_setopt(hnd, CURLOPT_URL, argv[1]);
+	curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, wm_callback);
+	curl_easy_setopt(hnd, CURLOPT_WRITEDATA, (void *)&chunk);
+	curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYPEER, 0L);
+	curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYHOST, 0L);
+	curl_easy_setopt(hnd, CURLOPT_FAILONERROR, 1L);
+	curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
+	curl_easy_setopt(hnd, CURLOPT_TIMEOUT_MS, TIMEOUT_MS);
+	curl_easy_setopt(hnd, CURLOPT_NOBODY, 1L);
+	curl_easy_setopt(hnd, CURLOPT_HEADER, 1L);
+	curl_easy_setopt(hnd, CURLOPT_USERAGENT, "curl/7.50.1");
+	curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 5L);
+	curl_easy_setopt(hnd, CURLOPT_FOLLOWLOCATION, 1L);
+	curl_easy_setopt(hnd, CURLOPT_FILETIME, 1L);
+	curl_easy_setopt(hnd, CURLOPT_TCP_KEEPALIVE, 1L);
+
+	ret = curl_easy_perform(hnd);
+
+	if(ret != CURLE_OK) {
+		fprintf(stderr, "Failed to fetch content-type, %s\n", curl_easy_strerror(ret));
+		curl_easy_cleanup(hnd);
+		return(EXIT_FAILURE);
+	}
+	
     int start = 0, mid = 0, end = 0;
-    // Use sentinels to grep out '/', then go back and forth to find endpoints, finally printing the range to stdout.
-    size_t i;
-    for (i = ct_strstr(chunk.memory); i < chunk.size; i++) {
-      if (chunk.memory[i] == '/') {
-        mid = i;
-        break;
-      }
-    }
+    
+	// Use sentinels to grep out '/', then go back and forth to find endpoints, finally printing the range to stdout.
+	size_t i;
+	
+	for (i = ct_strstr(chunk.memory); i < chunk.size; i++) {
+		if (chunk.memory[i] == '/') {
+			mid = i;
+			break;
+		}
+	}
 
-    for (i = mid + 1; i < chunk.size; i++) {
-      if (!isalnum(chunk.memory[i]) && chunk.memory[i] != '-') {
-        end = i;
-        break;
-      }
-    }
+	for (i = mid + 1; i < chunk.size; i++) {
+		if (!isalnum(chunk.memory[i]) && chunk.memory[i] != '-') {
+			end = i;
+			break;
+		}
+	}
 
-    for (i = mid - 1; i > 0; i--) {
-      if (!isalnum(chunk.memory[i]) && chunk.memory[i] != '-') {
-        start = ++i;
-        break;
-      }
-    }
+	for (i = mid - 1; i > 0; i--) {
+		if (!isalnum(chunk.memory[i]) && chunk.memory[i] != '-') {
+			start = ++i;
+			break;
+		}
+	}
 
-    printf("%.*s\n", end - start, &chunk.memory[start]);
-  }
+	printf("%.*s\n", end - start, &chunk.memory[start]);
 
-  curl_easy_cleanup(hnd);
-  hnd = NULL;
+	curl_easy_cleanup(hnd);
+	hnd = NULL;
 
-  return (int)ret;
+	return (int)ret;
+
 }
